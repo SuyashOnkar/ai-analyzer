9af2c45995c67a10b8b637f5913e81608adbb229
suyashonkar <suyashonkar@gmail.com>
2024-03-03 18:50:00 +0530

    basic setup is done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('search')
+  @UseInterceptors(FileInterceptor('file'))
+  async search(@UploadedFile() file: Express.Multer.File, @Body('query') query: string) {
+    return this.appService.performLLMSearch(file, query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Document, VectorStoreIndex } from 'llamaindex';
+import { config } from 'dotenv';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async performLLMSearch(file: Express.Multer.File, query: string): Promise<any> {
+    config();
+
+    const document = new Document({ text: file.buffer.toString(), id_: file.path });
+
+    const index = await VectorStoreIndex.fromDocuments([document]);
+
+    const queryEngine = index.asQueryEngine();
+    const response = await queryEngine.query({
+      query: query,
+    });
+
+    // Output response
+    return response.toString();
   }
 }
